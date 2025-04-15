9daf5d120883855dd703404ad78a3fe38ad8bdee
Laboratório FIAP <paulo7lima@hotmail.com>
2025-04-15 20:42:18 -0300

    Criacao da Produto.JAVA

---
+++ PastaFontes/Produto.java
@@ -0,0 +1,55 @@
+public class Produto {
+
+    // Atributos da classe
+    private String nome;
+    private double preco;
+    private int quantidadeEmEstoque;
+
+    // Construtor
+    public Produto(String nome, double preco, int quantidadeEmEstoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    // Métodos get (para acessar os valores)
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidadeEmEstoque() {
+        return quantidadeEmEstoque;
+    }
+
+    // Métodos set (para modificar os valores)
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidadeEmEstoque(int quantidadeEmEstoque) {
+        this.quantidadeEmEstoque = quantidadeEmEstoque;
+    }
+
+    // Método para mostrar os dados do produto
+    public void mostrarProduto() {
+        System.out.println("Produto: " + nome);
+        System.out.println("Preço: R$" + preco);
+        System.out.println("Quantidade em estoque: " + quantidadeEmEstoque);
+    }
+
+    public static void main(String[] args) {
+        // Criando um objeto Produto
+        Produto produto = new Produto("Camiseta", 39.90, 100);
+
+        // Mostrando os dados do produto
+        produto.mostrarProduto();
+
+        // Us
